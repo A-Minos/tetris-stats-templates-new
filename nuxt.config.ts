7524376f95374f77ac6ee5d0f1bca6ec69ff944b
shoucandanghehe <wallfjjd@gmail.com>
2024-11-03 23:57:35 +0800

    ➕ 添加依赖 nuxtjs/i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
     ssr: false,
     srcDir: 'src',
-    modules: ['@unocss/nuxt'],
+    modules: ['@unocss/nuxt', '@nuxtjs/i18n'],
     router: {
         options: {
             hashMode: true,
