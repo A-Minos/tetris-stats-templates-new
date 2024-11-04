f52a9e1b8e2a5d2a2f090f4b2f454e63f6d6c233
shoucandanghehe <wallfjjd@gmail.com>
2024-11-05 00:51:07 +0800

    ♻️ v1/binding 支持 i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,16 @@ export default defineNuxtConfig({
     ssr: false,
     srcDir: 'src',
     modules: ['@unocss/nuxt', '@nuxtjs/i18n'],
+
     router: {
         options: {
             hashMode: true,
         },
     },
+
     i18n: {
         vueI18n: './i18n.config.ts',
     },
+
+    compatibilityDate: '2024-11-04',
 });
