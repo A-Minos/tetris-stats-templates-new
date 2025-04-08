32ad7a76d82c6d02839880b494ac9db96e8f7471
shoucandanghehe <wallfjjd@gmail.com>
2025-04-08 18:33:23 +0800

    ✨ 添加响应式的formatDateFns方法

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -1,6 +1,29 @@
+import type { FormatDistanceToNowOptions } from 'date-fns';
+import { enUS, zhCN } from 'date-fns/locale';
 import { z } from 'zod';
 import Languages from '~/constants/enum/languages';
 import useData from '~/utils/useData';
+
+const dateFnsMapping = {
+    'zh-CN': zhCN,
+    'en-US': enUS,
+};
+
+export function formatDateFns(
+    formater: (options?: FormatDistanceToNowOptions) => string,
+    options?: FormatDistanceToNowOptions,
+): Ref<string> {
+    const nuxtApp = useNuxtApp();
+    const { locale } = useI18n();
+
+    const formatedText = ref(formater({ ...options, locale: dateFnsMapping[locale.value] }));
+
+    nuxtApp.hook('i18n:beforeLocaleSwitch', ({ newLocale }) => {
+        formatedText.value = formater({ ...options, locale: dateFnsMapping[newLocale] });
+    });
+
+    return formatedText;
+}
 
 export default defineNuxtPlugin((app) => {
     app.hooks.addHooks({
