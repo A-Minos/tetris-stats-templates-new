7bf495c1931a915ee918f458efcf56a1b6d4e11e
shoucandanghehe <wallfjjd@gmail.com>
2025-05-14 13:31:18 +0800

    ✨ 数据解析添加异常捕获

--- src/utils/useData.ts
+++ src/utils/useData.ts
@@ -3,5 +3,9 @@ import { z } from 'zod';
 export default <T extends z.ZodReadonly<z.ZodObject<any, any, any, any, any> | z.ZodUnion<any>>>(
     model: T,
 ): z.infer<T> => {
-    return model.parse(JSON.parse(window.__DATA__));
+    try {
+        return model.parse(JSON.parse(window.__DATA__));
+    } catch (e) {
+        console.error('Error parsing data:', e);
+    }
 };
