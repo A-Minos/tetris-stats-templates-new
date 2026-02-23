7baf141ae22d4ba93880a52f4648fca1d510df5f
shoucandanghehe <wallfjjd@gmail.com>
2026-02-23 18:32:35 +0800

    🩹 补充 vue-i18n 的  组件实例类型

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,4 +1,12 @@
+import type { ComposerTranslation } from 'vue-i18n';
+
 export {};
+
+declare module '@vue/runtime-core' {
+    interface ComponentCustomProperties {
+        $t: ComposerTranslation;
+    }
+}
 
 declare global {
     interface Window {
